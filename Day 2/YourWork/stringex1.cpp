e07ea52bd3daef60e89c6ee46d020d3c93faaf45
puspita022 <puspitapaul272@gmail.com>
2026-09-22 14:39:48 +0600

    Add files via upload

---
+++ Day 2/YourWork/stringex1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s; // Reads a single word (stops at space)
+    cout << "You entered: " << s << "\n";
+    
+    return 0;
+}
